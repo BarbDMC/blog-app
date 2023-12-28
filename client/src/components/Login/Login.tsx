5c5d91304d30f97b8539b9f961f02f8fd0d88fd9
BarbDMC <barbaradmorantesc@gmail.com>
2023-12-28 15:24:10 -0500

    Fix: Login styles moved to its own file

--- client/src/components/Login/Login.tsx
+++ client/src/components/Login/Login.tsx
@@ -1,4 +1,5 @@
 
+import './styles.css';
 import React, { useState } from "react";
 import { useLogin } from "src/hooks/useLogin";
 
@@ -14,26 +15,26 @@ export const Login: React.FC = () => {
   }
 
   return (
-    <section className="bg-gray-50 dark:bg-gray-900">
-      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-            <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-                <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+    <section className="login-background">
+      <div className="login-container-align">
+        <div className="login-container">
+            <div className="login-container-content">
+                <h1 className="login-container-content__title">
                     Sign in to your account
                 </h1>
-                <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
+                <form className="login-form" onSubmit={handleSubmit}>
                     <div>
-                        <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                        <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required={true} onChange={(e) => setEmail(e.target.value)} />
+                        <label htmlFor="email" className="login-form-label">Your email</label>
+                        <input type="email" name="email" id="email" className="login-form-input" placeholder="name@company.com" required={true} onChange={(e) => setEmail(e.target.value)} />
                     </div>
                     <div>
-                        <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                        <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required={true} onChange={(e) => setPassword(e.target.value)} />
+                        <label htmlFor="password" className="login-form-label">Password</label>
+                        <input type="password" name="password" id="password" className="login-form-input" placeholder="••••••••" required={true} onChange={(e) => setPassword(e.target.value)} />
                     </div>
-                    <button type="submit" className="w-full text-white bg-blue-700 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800" disabled={loading} >Sign in</button>
-                    {error && <p className="text-sm font-medium text-red-500">{error}</p>}
-                    <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                        Don’t have an account yet? <a href="#" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
+                    <button type="submit" className="login-form-button" disabled={loading} >Sign in</button>
+                    {error && <p className="login-error">{error}</p>}
+                    <p className="login-signup-message">
+                        Don’t have an account yet? <a href="#" className="login-signup-message__link">Sign up</a>
                     </p>
                 </form>
             </div>
@@ -42,3 +43,5 @@ export const Login: React.FC = () => {
     </section>
   )
 };
+
+//focus:ring-4 focus:outline-none focus:ring-primary-300 dark:focus:ring-blue-500 dark:focus:border-blue-500
