b5c32fa2b8a396e2a6f5c8ce6ad21c2b4a25cbe6
BarbDMC <barbaradmorantesc@gmail.com>
2023-12-28 16:22:17 -0500

    Feat: Sign up view

--- client/src/components/App/App.tsx
+++ client/src/components/App/App.tsx
@@ -1,5 +1,6 @@
 import Home  from "../Home/Home";
 import { Login } from "../Login/Login";
+import { SignUp } from "../SignUp/SignUp";
 import { Routes, Route } from "react-router-dom";
 
 
@@ -8,7 +9,7 @@ function App() {
     <div className="App">
        <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/signup" />
+        <Route path="/signup" element={<SignUp />}/>
         <Route path="/login" element={<Login />} />
       </Routes>
     </div>
