e461be234c9c1d6034a9eaa129ffec8d890a3a10
BarbDMC <barbaradmorantesc@gmail.com>
2024-02-01 19:09:45 -0500

    feat: add 404 page
    404 page was added

---
+++ client/src/pages/404/NotFound.tsx
@@ -0,0 +1,23 @@
+
+import './styles.css';
+import React from 'react';
+
+export const NotFound = () => {
+
+  return (
+    <section className="container-background">
+      <div className="container-content">
+        <div className="container-content--center">
+          <h1 className='container-content__title'>404: Not Found</h1>
+          <p className='container-content__description'>The page you are looking for does not exist.</p>
+        </div>
+
+        
+      </div>
+
+    </section>
+
+  );
+};
+
+export default NotFound;
--- client/src/pages/App/App.tsx
+++ client/src/pages/App/App.tsx
@@ -2,6 +2,7 @@ import Home  from "../Home/Home";
 import { Login } from "../Login/Login";
 import { SignUp } from "../SignUp/SignUp";
 import { Profile } from "../Profile/Profile";
+import { NotFound } from "../404/NotFound";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 
@@ -14,6 +15,7 @@ function App() {
           <Route path="/signup" element={<SignUp />}/>
           <Route path="/login" element={<Login />} />
           <Route path="/profile" element={<Profile />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </div>
