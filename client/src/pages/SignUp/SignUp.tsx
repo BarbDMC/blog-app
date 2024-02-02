73f80225fc1402763c98362a3e3d6fd3650eae21
BarbDMC <barbaradmorantesc@gmail.com>
2024-02-02 11:25:15 -0500

    feat(SignUp): use reducer and react form
    the signup page was change to use reducer and react form hooks to improve data handling

--- client/src/pages/SignUp/SignUp.tsx
+++ client/src/pages/SignUp/SignUp.tsx
@@ -1,67 +1,66 @@
-
-
 import './styles.css';
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
+import { SubmitHandler, useForm } from 'react-hook-form';
 import { useSignUp } from '../../hooks/useSignUp';
 import { useNavigate, Link } from 'react-router-dom';
+import { UserSignUpInterface } from 'src/interfaces/userInterfaces';
+
+const initialState = {
+  isLoading: false,
+  error: null,
+};
+
+type Action =
+  | { type: 'REQUEST' }
+  | { type: 'SUCCESS' }
+  | { type: 'FAILURE'; payload: string };
+
+type State = {
+  isLoading: boolean;
+  error: string | null;
+};
+
+
+function reducer(state: State, action: Action) {
+  switch (action.type) {
+  case 'REQUEST':
+    return { ...state, isLoading: true };
+  case 'SUCCESS':
+    return { ...state, isLoading: false };
+  case 'FAILURE':
+    return { ...state, isLoading: false, error: action.payload };
+  default:
+    throw new Error();
+  }
+}
 
 export const SignUp: React.FC = () => {
-  const [name, setName] = useState<string>("");
-  const [middleName, setMiddleName] = useState<string>("");
-  const [surname, setSurname] = useState<string>("");
-  const [secondSurname, setSecondSurname] = useState<string>("");
-  const [birthday, setBirthday] = useState<string>("");
-  const [email, setEmail] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
-  const { signUp, error, isLoading } = useSignUp();
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { signUp } = useSignUp();
   const navigate = useNavigate();
+  const { register, handleSubmit, formState: { errors } } = useForm();
 
-  const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setName(e.target.value);
+  const convertDate = (date: string) => {
+    return new Date(date).toISOString();
   };
 
-  const handleMiddleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setMiddleName(e.target.value);
-  };
+  const onSubmit: SubmitHandler<UserSignUpInterface> = async (data) => {
+    dispatch({ type: 'REQUEST' });
+    
+    try {
+      data.birthday = convertDate(data.birthday);
+      const createdUser = await signUp(data);
+      
+      if (createdUser) {
+        navigate('/profile', { state: createdUser });
+      }
 
-  const handleSurnameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSurname(e.target.value);
-  };
+      dispatch({ type: 'SUCCESS' });
 
-  const handleSecondSurnameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSecondSurname(e.target.value);
-  };
+    } catch (error) {
+      const message = (error instanceof Error) ? error.message : 'An unexpected error occurred';
+      dispatch({ type: 'FAILURE', payload: message });
 
-  const handleBirthdayChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const inputDate = new Date(e.target.value).toISOString();
-    setBirthday(inputDate);
-  };
-
-  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setEmail(e.target.value);
-  };
-
-  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setPassword(e.target.value);
-  };
-
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    const newUser = {
-      name: name,
-      middleName: middleName,
-      surname: surname,
-      secondSurname: secondSurname,
-      birthday: birthday,
-      email: email,
-      password: password,
-    };
-
-    const createdUser = await signUp(newUser);
-
-    if (createdUser) {
-      navigate('/profile', { state: createdUser });
     }
   };
 
@@ -71,53 +70,58 @@ export const SignUp: React.FC = () => {
         <div className="signup-container">
           <div className="signup-container-content">
             <h1 className="signup-container-content__title">
-                      Create an account
+              Create an account
             </h1>
-            <form data-testid='signUp-form' className="signup-form" onSubmit={handleSubmit}>
+            <form data-testid='signUp-form' className="signup-form" onSubmit={handleSubmit(onSubmit)}>
               <div className="signup-form-section">
                 <div>
                   <label htmlFor="name" className="signup-form-label">Name</label>
-                  <input className="signup-form-input" data-testid="name" type="text" placeholder="First Name" required={true} onChange={handleNameChange}/>
+                  <input className="signup-form-input" data-testid="name" type="text" placeholder="First Name" {...register("name", { required: true })} />
+                  {errors.name && <p className="signup-error-message">Name is required.</p>}
                 </div>
 
                 <div>
                   <label htmlFor="middleName" className="signup-form-label">Middle Name</label>
-                  <input className="signup-form-input" type="text" placeholder="Middle Name" onChange={handleMiddleNameChange} />
+                  <input className="signup-form-input" type="text" placeholder="Middle Name" {...register("middleName")} />
                 </div>
               </div>
 
               <div className="signup-form-section">
                 <div>
                   <label htmlFor="surname" className="signup-form-label">Surname</label>
-                  <input className="signup-form-input" data-testid="surname" type="text" placeholder="Surname" required={true} onChange={handleSurnameChange} />
+                  <input className="signup-form-input" data-testid="surname" type="text" placeholder="Surname" {...register("surname", { required: true })} />
+                  {errors.surname && <p className="signup-error-message">Surname is required.</p>}
                 </div>
 
                 <div>
                   <label htmlFor="secondSurname" className="signup-form-label">Second Surname</label>
-                  <input className="signup-form-input" type="text" placeholder="Second Surname" onChange={handleSecondSurnameChange} />
+                  <input className="signup-form-input" type="text" placeholder="Second Surname" {...register("secondSurname")} />
                 </div>
               </div>
 
               <div>
                 <label htmlFor="birthday" className="signup-form-label">Your Birth Day</label>
-                <input type="date" name="birthday" data-testid="birthday" placeholder='mm/dd/yyyy' className="signup-form-input" required={true} onChange={handleBirthdayChange} />
+                <input type="date" data-testid="birthday" placeholder='mm/dd/yyyy' className="signup-form-input" {...register("birthday", { required: true })} />
+                {state.error && <p className="signup-error-message">{state.error}</p>}
               </div>
 
               <div>
                 <label htmlFor="email" className="signup-form-label">Your email</label>
-                <input type="email" name="email" data-testid="email" className="signup-form-input" placeholder="name@company.com" required={true} onChange={handleEmailChange} />
+                <input type="email" data-testid="email" className="signup-form-input" placeholder="name@company.com" {...register("email", { required: true })} />
+                {errors.email && <p className="signup-error-message">Email is required.</p>}
               </div>
               <div>
                 <label htmlFor="password" className="signup-form-label">Password</label>
-                <input type="password" name="password" data-testid="password" placeholder="••••••••" className="signup-form-input" required={true} onChange={handlePasswordChange} />
+                <input type="password" data-testid="password" placeholder="••••••••" className="signup-form-input" {...register("password", { required: true })} />
+                {errors.password && <p className="signup-error-message">Password is required.</p>}
               </div>
 
-              <button type="submit" className="signup-form-button" disabled={isLoading}>Create an account</button>
+              <button type="submit" className="signup-form-button" disabled={state.isLoading} >Create an account</button>
 
-              {error && <p className="signup-error-message">{error}</p>}
+              {state.error && <p className="signup-error-message">{state.error}</p>}
 
               <p className="signup-login-message">
-                          Already have an account? <Link to="/login" className="signup-login-message__link">Login here</Link>
+                Already have an account? <Link to="/login" className="signup-login-message__link">Login here</Link>
               </p>
             </form>
           </div>
